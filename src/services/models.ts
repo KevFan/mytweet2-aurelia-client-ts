f4078fc922e2c396b16bbd7d01245fba602985fc
KevFan <kevin_fan@hotmail.co.uk>
2018-01-01 16:23:56 +0000

    changes models to match web api models

--- src/services/models.ts
+++ src/services/models.ts
@@ -1,21 +1,31 @@
-export interface Candidate {
-  firstName: string;
-  lastName: string;
-  office: string;
-  _id?: string;
-}
-
-export interface Donation {
-  amount: number;
-  method: string;
-  candidate: Candidate;
-  _id?: string;
-}
-
 export interface User {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
+  image: string;
+  _id?: string;
+}
+
+export interface Admin {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  _id?: string;
+}
+
+export interface Tweet {
+  tweetText: string;
+  tweetDate?: string;
+  tweetUser?: User;
+  tweetImage: string;
+  marker: {
+    id: number;
+    coords: {
+      latitude: number;
+      longitude: number;
+    };
+  };
   _id?: string;
 }
