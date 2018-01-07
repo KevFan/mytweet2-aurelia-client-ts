7abe84e8823abfbb36adeb091e6440614b626e71
KevFan <kevin_fan@hotmail.co.uk>
2018-01-07 11:40:38 +0000

    add use deployed server as base url

--- src/services/fixtures.ts
+++ src/services/fixtures.ts
@@ -2,5 +2,6 @@
  * Class containing data to be used by the application
  */
 export default class Fixtures {
-  baseUrl = 'http://localhost:4000';
+  // baseUrl = 'http://localhost:4000';
+  baseUrl = 'http://34.242.209.100';
 }
